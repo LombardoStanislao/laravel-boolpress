7c1f60b3670eb5b750c2d2ff8915dad4c94bb85b
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2021-01-27 19:20:58 +0100

    Mappatura relazione Category-Post

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['post_title', 'post_subtitle', 'post_text'];
+
+    public function category() {
+        return $this->belongsTo('App\Category');
+    }
 }
