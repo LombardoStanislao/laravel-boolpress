52b10fbe572734d05b2f0816819627df0b4cdd51
Stanislao <lombardo.stanislao94@xn--lieb-spa>
2021-01-28 15:31:15 +0100

    ticket

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Auth::routes();
 Route::middleware('auth')->prefix('admin')->namespace('Admin')->name('admin.')->group(function() {
     Route::get('/', 'HomeController@index')->name('index');
     Route::resource('/posts', 'PostController');
+    Route::resource('/categories', 'CategoryController');
 });
